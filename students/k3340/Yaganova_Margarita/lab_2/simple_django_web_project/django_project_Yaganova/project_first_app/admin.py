00baa8ee0d4bbf92bac1d54c5a86a692c7e73daf
m.yaganova <m.yaganova@yadro.com>
2024-11-04 23:58:14 +0300

    Delete lab_2

--- students/k3340/Yaganova_Margarita/lab_2/simple_django_web_project/django_project_Yaganova/project_first_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CarOwner, Car, Possesion, DriverLicence
-
-admin.site.register(CarOwner)
-admin.site.register(Car)
-admin.site.register(Possesion)
-admin.site.register(DriverLicence)
